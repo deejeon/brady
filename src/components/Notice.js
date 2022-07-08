72b9a7cdaca2546c9d9c181b6d430c62672d5803
Daniel Jeon <djeon93@gmail.com>
2022-07-08 16:19:26 -0700

    added Notice

---
+++ src/components/Notice.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Notice.css";
+
+function Notice() {
+    return (
+        <div className="notice-container">
+            <p className="notice-content">Get FREE delivery for orders above $250!</p>
+        </div>
+    )
+}
+
+export default Notice;
